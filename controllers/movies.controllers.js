564963a19b358f65ffb0b60691250dae1bb9219d
SarathMSK <sarathmsk92@gmail.com>
2024-09-24 08:46:58 +0530

    controller changes

---
+++ controllers/movies.controllers.js
@@ -0,0 +1,18 @@
+
+export const MovieGet = (req, res) => {
+    res.send("Get all movies!!")
+}
+
+
+export const MovieCreate = (req, res) => {
+    res.send("Create movies")
+}
+
+
+export const MovieUpdate = (req, res) => {
+    res.send("Update Movies")
+}
+
+export const MovieDelete = (req, res) => {
+    res.send("Delete movies")
+}
--- routes/movies.route.js
+++ routes/movies.route.js
@@ -1,26 +1,18 @@
 import express from "express"
+import { MovieCreate, MovieDelete, MovieGet, MovieUpdate } from "../controllers/movies.controllers";
 
 const router = express.Router();
 
 //  C - Creating movies
-router.post('/', (req, res) => {
-    res.send("Creating movies!!")
-})
+router.post('/', MovieCreate)
 
 // R - Reading
-router.get('/', (req, res) => {
-    res.send("Get all movies!!")
-})
-
+router.get('/', MovieGet)
 
 // U - Update
-router.put('/:id', (req, res) => {
-    res.send("Update movies")
-})
+router.put('/:id', MovieUpdate)
 
 // D - Delete
-router.delete('/:id', (req, res) => {
-    res.send("Delete movies")
-})
+router.delete('/:id', MovieDelete)
 
 export default router;
